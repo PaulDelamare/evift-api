4e061e6137523cb8e00b218a348bfb49dd6c09df
PaulDelamare <paulodela2006@gmail.com>
2024-06-09 17:53:07 +0200

    ✨ Start api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
+// Import
 import { Elysia } from "elysia";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// Variable
+// Get Port form .env
+const port = process.env.PORT!;
+
+
+const app = new Elysia()
+  .use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "api-key",
+        "accept",
+        "origin",
+        "x-requested-with",
+      ],
+    }),
+  )
+  .get("/", () => "Hello Elysia")
+  .listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
