76a3e00a58dfe6af2d05e94526e39e0d68ca19dc
PaulDelamare <paulodela2006@gmail.com>
2024-06-15 12:00:25 +0200

    ✏️ Add Comment to auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,10 @@
+// ! IMPORTS
 import { sendEmail } from "../email/sendEmail";
 import { errorServer } from "../lib/utils/errorServer";
 import { User } from "../models/User";
 import { PrismaClient } from '@prisma/client'
 
-
+// ! CLASS
 export class AuthController {
 
     // ! Class Variable
@@ -18,7 +19,10 @@ export class AuthController {
     // ! LOGIN
     public async login(body: { email: string; password: string }) {
 
+        // - Try Request
         try {
+
+            // Find User in Database with Email
             const user = await this.bdd.user.findUnique({
                 where: { email: body.email },
                 select: {
@@ -29,33 +33,39 @@ export class AuthController {
                     lastname: true
                 },
             });
-            
+
+            // Return Error if User not found
             if (!user) {
                 return { status: 400, error: "L'adresse email ou le mot de passe est incorrect" };
             }
-            
-            // match password
+
+            // Check if Password match
             const matchPassword = await Bun.password.verify(
                 body.password,
                 user.password
             );
-                
+
+            // Return Error if Password not match
             if (!matchPassword) {
                 return { status: 400, error: "L'adresse email ou le mot de passe est incorrect" };
             }
 
-            const {password, ...userWithoutPassword} = user
+            // Remove password from user
+            const { password, ...userWithoutPassword } = user
 
             // Return Success message 
             return {
                 status: 200,
-                data : userWithoutPassword
+                data: userWithoutPassword
             }
-            
-        } catch (error) {
+        }
 
+        // - Catch Error
+        catch (error) {
+
+            // Return Error Server
             return errorServer(error, "Une erreur s'est produite lors de la création de l'utilisateur");
-        } 
+        }
     }
 
     // ! REGISTER
@@ -73,7 +83,7 @@ export class AuthController {
 
             // Hash Password before creation
             const hashedPassword = await Bun.password.hash(body.password);
-      
+
             // Create User
             const newUser = await this.bdd.user.create({
                 data: {
@@ -84,22 +94,25 @@ export class AuthController {
                 },
             });
 
+            // Cretae email Data 
             const emailData = {
                 firstname: newUser.firstname,
                 emailService: process.env.EMAIL_SERVICE
             }
 
-            await sendEmail(newUser.email,  process.env.EMAIL_SENDER!, 'Création d\'un compte Evift', 'validateEmail/validate-success', emailData );
-      
+            // Send Email
+            await sendEmail(newUser.email, process.env.EMAIL_SENDER!, 'Création d\'un compte Evift', 'validateEmail/validate-success', emailData);
+
             // Return Success message 
             return {
                 status: 201,
-                error : "Utilisateur enregisté avec succès !"
+                error: "Utilisateur enregisté avec succès !"
             }
 
-        // ? If an error occur
+            // ? If an error occur
         } catch (error: unknown) {
 
+            // Return Error Server
             return errorServer(error, "Une erreur s'est produite lors de la création de l'utilisateur");
 
         }
