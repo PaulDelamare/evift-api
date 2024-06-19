f3c285e7f2f272173b8e7ff8711072c52e222bab
Paul_Delamare <paulodela2006@gmail.com>
2024-06-19 15:38:36 +0200

    ➕ ➕ Use User route and Invite route

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
 // ! IMPORTS
 import { Elysia } from "elysia";
 import cors from "@elysiajs/cors";
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
 import { auth } from "./routes/auth.routes";
+import { user } from "./routes/user.routes";
+import { invite } from "./routes/invitation.routes";
 import { checkApiKey } from "./plugins/checkApiKey";
-
 
 // Variable
 // Get Port form .env
 const port = process.env.PORT!;
 
-// ! INSTANCE 
+// ! INSTANCE
 const app = new Elysia()
 
   // ! SWAGGER
-  .use(swagger({
-    provider: "swagger-ui", documentation: {
-      tags: [
-        { name: 'Auth', description: 'Authentication request' },
-      ]
-    }
-  }))
-    // ! HELLO WORLD !
-    .get("/", ({headers}) =>{ console.log(headers);return {name: "Hello Elysia"}})
+  .use(
+    swagger({
+      provider: "swagger-ui",
+      documentation: {
+        tags: [{ name: "Auth", description: "Authentication request" }],
+      },
+    })
+  )
+  // ! HELLO WORLD !
+  .get("/", ({ headers }) => {
+    console.log(headers);
+    return { name: "Hello Elysia" };
+  })
 
   // ! API KEY
   .use(checkApiKey)
@@ -40,14 +45,15 @@ const app = new Elysia()
         "origin",
         "x-requested-with",
       ],
-    }),
+    })
   )
 
   // ! GROUP ROUTES
-  .group('/api', (app) =>
-
+  .group("/api", (app) =>
     // - Auth routes
-    app.use(auth),
+    app.use(auth)
+        .use(user)
+        .use(invite)
   )
 
   // ! RUN SERVER
