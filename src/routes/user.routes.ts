c29b1a40ccacdbae511beb65730a0f8f4da74016
Paul_Delamare <paulodela2006@gmail.com>
2024-06-19 16:03:35 +0200

    ✨ ➕ Create User route and Add get user by email

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,56 @@
+// ! IMPORTS
+import { Elysia, t } from "elysia";
+import { authPlugin } from "../plugins/jwtAuth/authPlugin";
+
+import { jwtConfig } from "../plugins/jwtAuth/jwtConfig";
+import { UserController } from "../controllers/user.controller";
+
+// Create User Route
+export const user = new Elysia({ prefix: "/user" })
+    // ! CONFIGURATION
+
+    // Declare controller Class
+    .decorate("userController", new UserController())
+
+    // ! Error Handler
+    .onError(({ code, error }) => {
+        // If Error is an instance of ValidationError
+        if (code === "VALIDATION")
+            // Throw Error
+            return { status: error.status, error: error };
+    })
+
+    // ? Use jwtConfig
+    .use(jwtConfig)
+
+    // HANDLER
+
+    // ? Use Plugin for check if user is logged
+    .use(authPlugin)
+
+        // ! ROUTES
+
+    // ? Get user by email
+    .get(
+        "/findUser/:email",
+        async ({ userController, params: { email }, set }) => {
+            console.log(email);
+
+            // get Response from register method
+            const response = await userController.findByEmail(email);
+
+            // Set status with status Reponse
+            set.status = response.status;
+
+            // Return response
+            return response;
+        },
+        {
+            params: t.Object({
+                email: t.String({
+                    format: "email",
+                    error: "L'adresse email est invalide",
+                }),
+            }),
+        }
+    );
