28d5512bb7af92bb4e22f132c315702853644da7
Paul_Delamare <paulodela2006@gmail.com>
2024-06-20 21:23:16 +0200

    ✨ ➕ ➕ ➕ Create Invitation Controller and Add Invite, Find and Accept/Refuse method

--- src/controllers/invitation.controller.ts
+++ src/controllers/invitation.controller.ts
@@ -140,4 +140,138 @@ export class InvitationController {
             );
         }
     }
+
+    // - Find Invitations
+    /**
+     * A function to find invitations based on the provided id.
+     *
+     * @param id - The id used to search for invitations.
+     * @return An object containing the status and invitations data.
+     */
+    public async findInvitations(id: string) {
+
+        // - Try Request
+        try {
+
+            // Find All Request for user
+            const invitations = await this.bdd.invitation.findMany({
+                where: {
+                    requestId: id,
+                },
+                // Don't return the password
+                include: {
+                    user: {
+                        select: {
+                            id: true,
+                            email: true,
+                            firstname: true,
+                            lastname: true
+                        },
+                    },
+                },
+            });
+
+            // Return Success message
+            return {
+                status: 200,
+                data: invitations
+            };
+
+        }
+        // ? Catch Error 
+        catch (error) {
+
+            // Return Error Server
+            return errorServer(error, "Une erreur s'est produite lors de la recherche des invitations");
+        }
+    }
+
+
+    // - Accept or Refuse Friend Invitation
+    /**
+     * Accepts or refuses a friend invitation.
+     *
+     * @param id - The ID of the invitation.
+     * @param userId - The ID of the user.
+     * @param res - The response (true for accept, false for refuse).
+     * @return - A promise that resolves to an object with the status code and optional error or success message.
+     */
+    public async acceptInvitation(id: string, userId: string, res: boolean) {
+
+        // - Try Request
+        try {
+
+            // Find Invitation in Database
+            const invitation = await this.bdd.invitation.findFirst({
+                where: {
+                    id: id
+                }
+            })
+
+            // If Invitation not found
+            if (!invitation) {
+                // Return Error
+                return { status: 404, error: "Invitation introuvable" };
+            }
+
+            //  If User is not the request
+            if (invitation.requestId !== userId) {
+                // Return Error
+                return { status: 400, error: "Vous ne poyvez pas réaliser cette action" }
+            }
+
+            // - If User is already friend
+            const alreadyFriend = await this.bdd.friends.findFirst({
+                where: {
+                    OR: [
+                        {
+                            user1Id: userId,
+                            user2Id: invitation.userId,
+                        },
+                        {
+                            user1Id: invitation.userId,
+                            user2Id: userId,
+                        },
+                    ],
+                },
+            });
+
+            // If User is already friend
+            if (alreadyFriend) {
+                // Return Error
+                return {
+                    status: 400,
+                    error: "Vous êtes déjà amis avec cet utilisateur",
+                };
+            }
+
+            // If Response is true
+            if (res === true) {
+
+                // Create Friend in DB
+                await this.bdd.friends.create({
+                    data: {
+                        user1Id: userId,
+                        user2Id: invitation.userId,
+                    },
+                });
+            }
+
+            // And Delete request
+            await this.bdd.invitation.delete({
+                where: {
+                    id: id
+                }
+            })
+
+            // Return Success
+            return { status: 201, message: "Invitation acceptée avec succès !" };
+
+        }
+        // - Catch Error
+        catch (error) {
+            // Return Error Server
+            return errorServer(error, "Une erreur s'est produite lors de la recherche des invitations");
+        }
+    }
 }
