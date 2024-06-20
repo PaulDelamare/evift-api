fef3bf298c0291787354e6788a018c1868d1a39d
Paul_Delamare <paulodela2006@gmail.com>
2024-06-20 21:26:23 +0200

    ✨ Create routes for Invitation

--- src/routes/invitation.routes.ts
+++ src/routes/invitation.routes.ts
@@ -62,5 +62,88 @@ export const invite = new Elysia({ prefix: "/invitation" })
                     error: "L'adresse email est invalide",
                 }),
             }),
+            detail: {
+                tags: ['Invitation'],
+                summary: 'Send request for add friend to an other user'
+            }
         }
-    );
+    )
+
+    // ? Find all Friends Request for our account
+    .get(
+        // - Path
+        "/findAll",
+
+        // - Function
+        async ({ body, set, invitationController, user }) => {
+
+            // Define user as User type
+            const userData = user! as User;
+
+            // get Response from invitationController
+            const response = await invitationController.findInvitations(
+                userData.id
+            );
+
+            // Set status with status Reponse
+            set.status = response.status;
+
+            // Return response
+            return response;
+        },
+
+        // - VALIDATION
+        {
+            detail: {
+                tags: ['Invitation'],
+                summary: 'Send request for add friend to an other user'
+            }
+        }
+    )
+
+    // ? Valide or refuse the Friends request
+
+    .post(
+        // - Path
+        "/accept",
+
+        // - Function
+        async ({ body, set, invitationController, user }) => {
+
+            // Define user as User type
+            const userData = user! as User;
+
+            // get Response from invitationController
+            // For add friend or just delete request
+            const response = await invitationController.acceptInvitation(
+                body.id,
+                userData.id,
+                body.response
+            );
+
+            // Set status with status Reponse
+            set.status = response.status;
+
+            // Return response
+            return response;
+        },
+        {
+            // Define body condition
+            body: t.Object({
+                // Id must be and uuid
+                id: t.String({
+                    format: "uuid",
+                    error: "L'id est invalide",
+                }),
+                // Response must me an boolean (tru for accept or false for refuse)
+                response: t.Boolean({
+                    error: "La réponse doit est true ou false",
+                }),
+            }),
+            // Add this to invitation swagger
+            detail: {
+                tags: ['Invitation'],
+                summary: 'Send request for add friend to an other user'
+            }
+        }
+    )
