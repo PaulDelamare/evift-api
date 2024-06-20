77a894692d6f4da385b6795765ad49547cebeea0
Paul_Delamare <paulodela2006@gmail.com>
2024-06-20 21:14:06 +0200

    ➕ Add new routes and swagger

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { swagger } from "@elysiajs/swagger";
 import { auth } from "./routes/auth.routes";
 import { user } from "./routes/user.routes";
 import { invite } from "./routes/invitation.routes";
+import {friends} from "./routes/friends.routes";
 import { checkApiKey } from "./plugins/checkApiKey";
 
 // Variable
@@ -19,7 +20,11 @@ const app = new Elysia()
     swagger({
       provider: "swagger-ui",
       documentation: {
-        tags: [{ name: "Auth", description: "Authentication request" }],
+        tags: [
+          { name: "Auth", description: "Authentication request" },
+          { name: "User", description: "User request" },
+          { name: "Invitation", description: "Friends Invitation request" },
+        ],
       },
     })
   )
@@ -56,6 +61,8 @@ const app = new Elysia()
       .use(user)
       // Invitation route
       .use(invite)
+      // Friends route
+      .use(friends)
   )
 
   // ! RUN SERVER
