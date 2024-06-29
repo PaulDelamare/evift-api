79ed1f0b2657e963aab0617b2e056346c5728e0f
PaulDelamare <paulodela2006@gmail.com>
2024-06-29 20:53:48 +0200

    ✨ Create event routes

---
+++ src/routes/event.routes.ts
@@ -0,0 +1,71 @@
+// ! IMPORTS
+import { Elysia, t } from "elysia";
+import { authPlugin } from "../plugins/jwtAuth/authPlugin";
+import { jwtConfig } from "../plugins/jwtAuth/jwtConfig";
+import { User } from "../models/User";
+import { eventModel } from "../models/Event";
+import { EventController } from "../controllers/event.controller";
+
+// Create Invitaion Route
+export const event = new Elysia({ prefix: "/event" })
+     // ! CONFIGURATION
+     // Declare controller Class
+     .decorate("eventController", new EventController())
+
+     // ! Error Handler
+     .onError(({ code, error }) => {
+          // If Error is an instance of ValidationError
+          if (code === "VALIDATION")
+               // Throw Error
+               return { status: error.status, error: error.message };
+     })
+
+     // ? Use jwtConfig
+     .use(jwtConfig)
+
+     // HANDLER
+
+     // ? Use Plugin for check if user is logged
+     .use(authPlugin)
+
+     // Import model for user
+     .use(eventModel)
+
+     // ! ROUTES
+
+     // ? Post request friends invitation
+     .post(
+          // - Path
+          "/create",
+
+          // - Function
+          async ({ body, set, eventController, user }) => {
+               console.log(body)
+
+               // Define user as User type
+               const userData = user! as User;
+
+               // get Response from invitationController
+               const response = await eventController.create(
+                    userData.id,
+                    body
+               );
+
+               // Set status with status Reponse
+               // set.status = response.status;
+               set.status = 200;
+
+               // Return response
+               return {test: 'ouaius ouais ouais'};
+          },
+
+          // - VALIDATION
+          {
+               detail: {
+                    tags: ['Event'],
+                    summary: 'Request for create event'
+               },
+               body: 'create',
+
+          }
+     );
