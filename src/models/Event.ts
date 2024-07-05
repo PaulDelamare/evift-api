031d9e767484114ec2c78038a302f363619f7503
PaulDelamare <paulodela2006@gmail.com>
2024-07-05 11:58:37 +0200

    ➕ Add time validation

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -20,6 +20,7 @@ export const eventModel = new Elysia()
                description: t.String({ minLength: 3, maxLength: 300, error: 'La description est invalide, il doit être une chaine de caractère entre 3 et 300 caractères' }),
                address: t.String({ minLength: 3, maxLength: 300, error: 'L\'adresse est invalide, il doit être une chaine de caractère entre 3 et 300 caractères' }),
                date: t.Date({ error: 'La date est invalide', minimumTimestamp: Date.now() }),
-               img: t.Optional(t.File({ error: 'L\'image est invalide', maxSize: 10485760, types: ['image/png', 'image/jpeg', 'image/jpg'] }))
+               img: t.Optional(t.File({ error: 'L\'image est invalide', maxSize: 10485760, types: ['image/png', 'image/jpeg', 'image/jpg'] })),
+               time: t.String({ error: 'L\'heure est invalide', regex: /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/ })
           })
      })
