016c36d52aa5b50e5095a0b78d5a388affa182c0
Paul_Delamare <paulodela2006@gmail.com>
2024-07-11 11:41:00 +0200

    ✨ Create script for create role

---
+++ src/script/createRole.script.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+/**
+ * Asynchronously creates multiple role events in the database and logs the created roles.
+ *
+ * @return A promise that resolves when the role events are created and logged.
+ * @throws If there is an error creating the role events.
+ */
+async function main() {
+
+    try {
+        await prisma.roleEvent.createMany({
+            data: [
+                {
+                    name: "admin",
+                },
+                {
+                    name: "participant",
+                },
+                {
+                    name: "gift",
+                },
+            ],
+        })
+
+        const roles = await prisma.roleEvent.findMany();
+        console.log(roles);
+    } catch (error) {
+        throw error
+    }
+
+}
+
+try {
+    await main();
+
+} catch (error) {
+    console.log("Une erreur c'est produit, les rôles sont peut être déjà crées");
+    process.exit(1);
+} finally {
+    await prisma.$disconnect();
+}
