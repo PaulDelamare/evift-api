0e63a7be08e2ed1f011c8027c9fbe18737a9012e
PaulDelamare <paulodela2006@gmail.com>
2025-06-08 12:48:04 +0200

    Nettoyage du code : suppression de commentaires superflus pour améliorer la lisibilité des routes d'authentification.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,88 +7,64 @@ import { ACCESS_TOKEN_EXP } from "../config/auth-config";
 import { getExpTimestamp } from "../lib/utils/getExpTimestamp";
 import { jwtConfig } from "../plugins/jwtAuth/jwtConfig";
 
-// Create Authentication Route
 export const auth = new Elysia({ prefix: "/auth" })
   // ! CONFIGURATION
+
   // Import model for user
   .use(userModel)
+
   // Declare controller Class
   .decorate('authController', new AuthController())
 
   // ! Error Handler
   .onError(({ code, error }) => {
-    // If Error is an instance of ValidationError
     if (code === 'VALIDATION')
-      // Throw Error
       return {  status: error.status, error: error };
   })
 
-  // ? Use jwtConfig
   .use(jwtConfig)
 
   // ! ROUTES
-  //? Create login route
   .post(
-    // Accessible with /login path
     "/login",
 
-    // Function
     async ({ authController, body, jwt, cookie: { accessToken }, set }) => {
 
-      // Controller Method
       const response = await authController.login(body);
 
-      // Set status with status Reponse
       set.status = response.status;
 
-      // If Error in response
       if ('error' in response) {
-        // Throw Error
         return response;
-        // throw new Error(response.error);
       }
 
-      // Get user from response
       const user = response.data;
 
       // create access token
       const accessJWTToken = await jwt.sign({
-        // Use user.id
         sub: user.id,
-        // Set exp time
         exp: getExpTimestamp(ACCESS_TOKEN_EXP),
       });
 
       // set access token cookie
       accessToken.set({
-        // Use accessJWTToken
         value: accessJWTToken,
-        // Declare cookie options
         httpOnly: true,
-        // Set exp time
         maxAge: ACCESS_TOKEN_EXP,
-        // Accesible for all routes
         path: "/",
       });
 
-      // Return response
       return {
-        // Success message
         message: "Connexion réussie",
-        // Return data
         data: {
-          // Return user
           user: user,
-          // Return access token
           accessToken: accessJWTToken,
           status: 200
         },
       };
     },
     {
-      // Use Login verification
       body: 'login',
-      // Detail for swagger
       detail: {
         tags: ['Auth'],
         summary: 'For login register'
@@ -96,34 +72,24 @@ export const auth = new Elysia({ prefix: "/auth" })
     }
   )
 
-  //? Create register route
   .post(
-    // Accessible with /register path
     "/register",
-    // CONTROLLER
     async ({ authController, body, set }) => {
 
-      // get Response from register method
       const response = await authController.register(body);
 
-      // Set status with status Reponse
       set.status = response.status;
 
-      // Return response
       return response;
     },
 
-    // HANDLER
     {
-      //- Validation Based on model
       body: 'user',
-      //- Detail for swagger
       detail: {
         tags: ['Auth'],
         summary: 'For login user'
       }
     })
-  // .use(authPlugin)
 
   // ? Use Plugin for check if user is logged 
   .use(authPlugin)
@@ -131,8 +97,6 @@ export const auth = new Elysia({ prefix: "/auth" })
   // ? Get current user
   .get('/me', ({ user }) => {
 
-
-    // Return user
     return user
   },
     {
