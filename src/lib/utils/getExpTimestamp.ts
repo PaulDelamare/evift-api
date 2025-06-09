71003bc41bd795b19e9adfeacfbb8ec4a3e66624
PaulDelamare <paulodela2006@gmail.com>
2025-06-09 14:38:27 +0200

    Suppression de commentaires superflus dans la fonction getExpTimestamp pour améliorer la lisibilité du code.

--- src/lib/utils/getExpTimestamp.ts
+++ src/lib/utils/getExpTimestamp.ts
@@ -5,16 +5,12 @@
  * @return The expiration timestamp in seconds.
  */
 function getExpTimestamp(seconds: number) {
-  // Get current time in milliseconds
   const currentTimeMillis = Date.now();
-  // Convert seconds into milliseconds
   const secondsIntoMillis = seconds * 1000;
-  // Calculate expiration time in milliseconds
   const expirationTimeMillis = currentTimeMillis + secondsIntoMillis;
 
   // Return expiration time in seconds
   return Math.floor(expirationTimeMillis / 1000);
 }
 
-// Export
 export { getExpTimestamp };
