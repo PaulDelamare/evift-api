70381f574ef4d26a435a71bb1540cbea6836b920
PaulDelamare <paulodela2006@gmail.com>
2025-06-09 14:35:36 +0200

    Réorganisation des imports et suppression de la configuration Swagger pour simplifier le fichier index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 // ! IMPORTS
 import { Elysia, t } from "elysia";
-import cors from "@elysiajs/cors";
-import { swagger } from "@elysiajs/swagger";
 import { auth } from "./routes/auth.routes";
 import { user } from "./routes/user.routes";
 import { invite } from "./routes/invitation.routes";
@@ -9,103 +7,71 @@ import { friends } from "./routes/friends.routes";
 import { event } from "./routes/event.routes";
 import { gift } from "./routes/gift.routes";
 import { rolesEvent } from "./routes/rolesEvent.routes";
-import { message } from "./routes/webSocket/message.routes";
+import { apiConfig } from "./config/api-config";
 import { checkApiKey } from "./plugins/checkApiKey";
+import { message } from "./routes/webSocket/message.routes";
 
-// Variable
-// Get Port form .env
 const port = process.env.PORT!;
 
 // ! INSTANCE
 const app = new Elysia()
+     .use(apiConfig)
 
-  // ! SWAGGER
-  .use(
-    swagger({
-      provider: "swagger-ui",
-      documentation: {
-        tags: [
-          { name: "Auth", description: "Authentication request" },
-          { name: "User", description: "User request" },
-          { name: "Invitation", description: "Friends Invitation request" },
-          { name: "Event", description: "All the request link to the event gestion" },
-          { name: "Role", description: "All the request link to the role gestion" },
-          { name: "Gift", description: "All the request link to the gift gestion" },
-          { name: "Image", description: "Request for get Image from API" },
-          { name: "Friends", description: "All the request link to the friends gestion" },
-        ],
-      },
-    })
-  )
+     .onError(({ code, error }) => {
+          if (code === 'VALIDATION') {
+               interface ValidationError {
+                    path: string;
+                    schema: {
+                         error?: string;
+                         [key: string]: unknown;
+                    };
+                    [key: string]: unknown;
+               }
 
-  // ! IMAGE
-  // Get the image
-  .get('/image', ({ query }) => Bun.file(query.name), {
-    query: t.Object({
-      name: t.String()
-    }),
-    detail: {
-      tags: ['Image'],
-      summary: 'Create sipple path for get Image form API'
-    },
-  })
-  // ! HELLO WORLD !
-  .get("/", ({ }) => {
-    return { name: "Hello Elysia" };
-  })
+               interface ErrorObject {
+                    error: string;
+               }
 
-  // ! WEBSOCKET
-  .use(message)
+               const errorsObject = (error.all as ValidationError[]).reduce(
+                    (acc: Record<string, ErrorObject>, curr: ValidationError) => {
+                         const key = 'path' in curr ? curr.path.replace(/^\//, '') : '';
+                         acc[key] = {
+                              error:
+                                   'schema' in curr && curr.schema.error !== undefined
+                                        ? String(curr.schema.error)
+                                        : 'Unknown error',
+                         };
+                         return acc;
+                    },
+                    {}
+               );
+               return {
+                    error: errorsObject
+               };
+          }
+     })
 
+     .use(message)
 
-  // ! API KEY
-  .use(checkApiKey)
+     .get("/", ({ }) => {
+          return { name: "Hello Elysia" };
+     })
 
-  // ! CORS
+     .group("/api", (app) =>
 
-  .use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "x-api-key",
-        "accept",
-        "origin",
-        "x-requested-with",
-      ],
-    })
-  )
+          app
+               .use(checkApiKey)
+               .use(auth)
+               .use(user)
+               .use(invite)
+               .use(friends)
+               .use(event)
+               .use(rolesEvent)
+               .use(gift)
+     )
 
-  // ! GROUP ROUTES
-  .group("/api", (app) =>
-
-    // - Auth routes
-    // Auth route
-    app.use(auth)
-      // User route
-      .use(user)
-
-      // Invitation route
-      .use(invite)
-
-      // Friends route
-      .use(friends)
-
-      // Events route
-      .use(event)
-
-      // Role route
-      .use(rolesEvent)
-
-      // Gifts routes
-      .use(gift)
-  )
-
-  // ! RUN SERVER
-  .listen(port);
+     .listen(port);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
