1b841ee73f7a345cbe15a13d69509358baa9324d
Paul_Delamare <paulodela2006@gmail.com>
2025-06-26 15:31:59 +0200

    Ajout des méthodes create et findAll dans RoleEventServices pour gérer la création et la récupération des événements de rôle.

--- src/services/roleEvent/roleEvent.services.ts
+++ src/services/roleEvent/roleEvent.services.ts
@@ -17,4 +17,29 @@ export class RoleEventServices extends BaseService {
 
           return await this.db.roleEvent.findFirst({ where: { name } })
      }
+
+     /**
+      * Creates a new role event with the specified name.
+      *
+      * @param name - The name of the role event to create.
+      * @returns A promise that resolves when the role event has been created.
+      */
+     public async create(name: string) {
+          await this.db.roleEvent.create({
+               data: {
+                    name: name
+               },
+          });
+     }
+
+     /**
+     * Retrieves all role events from the database.
+     *
+     * @returns A promise that resolves to an array of role event objects.
+     * @throws Will throw an error if the database query fails.
+     */
+     public async findAll() {
+          const roles = await this.db.roleEvent.findMany();
+          return roles
+     }
 }
