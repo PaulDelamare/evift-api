5f23758b4e4af6e06255b2a94e1173c02bfe0ada
Paul_Delamare <paulodela2006@gmail.com>
2025-06-26 15:31:48 +0200

    Refactor rolesEvent.routes.ts en supprimant le contrôleur de rôle et en intégrant les services de rôle pour simplifier le code et améliorer la gestion des erreurs.

--- src/routes/rolesEvent.routes.ts
+++ src/routes/rolesEvent.routes.ts
@@ -1,104 +1,38 @@
-// ! IMPORTS
 import { Elysia, t } from "elysia";
 import { authPlugin } from "../plugins/jwtAuth/authPlugin";
 import { jwtConfig } from "../plugins/jwtAuth/jwtConfig";
 import { RolesController } from "../controllers/roles.controller";
+import { RoleEventServices } from "../services/roleEvent/roleEvent.services";
+import { sendResponse } from "../lib/utils/returnSuccess/returnSuccess";
+import { handleError } from "../lib/utils/errorHandler/errorHandler";
 
-// Create RoleEvent Route
 export const rolesEvent = new Elysia({ prefix: "/rolesEvent" })
-     // ! CONFIGURATION
-     // Declare controller Class
      .decorate("roleController", new RolesController())
+     .decorate("roleServices", new RoleEventServices())
 
-     // ! Error Handler
-     .onError(({ code, error }) => {
-          // If Error is an instance of ValidationError
-          if (code === "VALIDATION")
-               // Throw Error
-               return { status: error.status, error: error.message };
-     })
 
-     // ? Use jwtConfig
      .use(jwtConfig)
-
-     // HANDLER
-
-     // ? Use Plugin for check if user is logged
      .use(authPlugin)
 
-     // ! ROUTES
-
-     // ? Create new role Event
-     // ! TO USE LATER WHEN OTHER REAL ROLE WILL BE CREATED
-     // .post(
-     //      // - Path
-     //      "/create",
-
-     //      // - Function
-     //      async ({ body, set, roleController }) => {
-
-     //           // get Response from invitationController
-     //           const response = await roleController.create(
-     //                body.name
-     //           );
-
-     //           // Set status with status Reponse
-     //           set.status = response.status;
-
-     //           // Return response
-     //           return response;
-     //      },
-
-     //      // - VALIDATION
-     //      {
-     //           detail: {
-     //                tags: ['Role'],
-     //                summary: 'Request for create new role for user in the event'
-     //           },
-     //           body: t.Object({
-     //                name: t.String()
-     //           }),
-     //      }
-     // )
-
      .get(
-          // - Path
           "/findAll",
 
-          // - Function
-          async ({ body, set, roleController }) => {
+          async (ctx) => {
+               try {
 
-               // get Response from invitationController
-               const response = await roleController.findAll();
+                    const roles = await ctx.roleServices.findAll();
+                    return sendResponse(ctx, 200, roles);
 
-               // Set status with status Reponse
-               set.status = response.status;
+               } catch (error) {
 
-               // Return response
-               return response;
+                    const { status, error: errorResponse } = handleError(error);
+                    throw ctx.error(status, errorResponse);
+               }
           },
-
-          // - VALIDATION
           {
-               response: {
-                    200: t.Object({
-                         data: t.Array(t.Object({
-                              id: t.String(),
-                              name: t.String(),
-                              createdAt: t.Date()
-                         })),
-                         status: t.Number(),
-                    }),
-                    500: t.Object({
-                         status: t.Number(),
-                         error: t.String(),
-                    })
-               },
-
                detail: {
                     tags: ['Role'],
                     summary: 'Request for get all event role',
                }
-
           }
      )
