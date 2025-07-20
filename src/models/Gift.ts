92f8d759778bfbbde1c3de1c5f821fa49f25a362
PaulDelamare <paulodela2006@gmail.com>
2025-07-20 14:53:19 +0200

    Corriger la déclaration de l'interface CreateListGift et modifier le type de l'URL en Nullable

--- src/models/Gift.ts
+++ src/models/Gift.ts
@@ -1,7 +1,7 @@
 // auth.model.ts
 import { Elysia, t } from 'elysia'
 
-export interface CreateListGift{
+export interface CreateListGift {
      name: string;
      gifts: {
           name: string;
@@ -17,7 +17,15 @@ export const giftModel = new Elysia()
                gifts: t.Array(t.Object({
                     name: t.String({ minLength: 2, maxLength: 100, error: 'Le nom est invalide, il doit être une chaine de caractère entre 2 et 100 caractères' }),
                     quantity: t.Number({ error: 'La quantité est invalide', min: 1, max: 100 }),
-                    url: t.Optional(t.String({ error: 'L\'url est invalide, il doit être une chaine de caractère entre 2 et 100 caractères' })),
+                    url: t.Nullable(t.String({ error: 'L\'url est invalide, il doit être une chaine de caractère entre 2 et 100 caractères' })),
+               })),
+          }),
+          addGift: t.Object({
+               id: t.String({ format: "uuid", errors: "L'id de la liste de cadeaux n'est pas valide" }),
+               gifts: t.Array(t.Object({
+                    name: t.String({ minLength: 2, maxLength: 100, error: 'Le nom est invalide, il doit être une chaine de caractère entre 2 et 100 caractères' }),
+                    quantity: t.Number({ error: 'La quantité est invalide', min: 1, max: 100 }),
+                    url: t.Nullable(t.String({ error: 'L\'url est invalide, il doit être une chaine de caractère entre 2 et 100 caractères' })),
                })),
           })
      })
