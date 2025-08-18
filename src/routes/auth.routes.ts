b3b143c3aea3d57677f5543e336b23b1892050da
PaulDelamare <paulodela2006@gmail.com>
2025-08-18 21:22:50 +0200

    Ajout des routes pour la réinitialisation de mot de passe, y compris la génération de lien, la vérification de la demande et le changement de mot de passe.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { userModel } from "../models/User";
 import { authPlugin } from "../plugins/jwtAuth/authPlugin";
 import { ACCESS_TOKEN_EXP } from "../config/auth-config";
@@ -95,7 +95,94 @@ export const auth = new Elysia({ prefix: "/auth" })
                }
           })
 
+     .post(
+          '/generatePasswordReset',
 
+          async (ctx) => {
+               try {
+                    const { email } = ctx.body;
+
+                    const { message } = await ctx.authServices.generatePasswordReset({ email: email.toLowerCase() });
+
+                    return sendResponse(ctx, 200, message);
+
+               } catch (err) {
+                    const { status, error: errorResponse } = handleError(err);
+
+                    throw ctx.error(status, errorResponse);
+               }
+          },
+          {
+               body: t.Object({
+                    email: t.String({ format: "email", errors: "L'adresse e-mail n'est pas valide" })
+               }),
+               detail: {
+                    tags: ['Auth'],
+                    summary: 'Request to generate a password reset link',
+               }
+          }
+     )
+     .get(
+          '/checkUserRequest',
+          async (ctx) => {
+
+               try {
+
+                    await ctx.authServices.checkUserRequest(ctx.query);
+
+                    return sendResponse(ctx, 200, { success: true, message: "Demande de réinitialisation de mot de passe valide" });
+
+               } catch (err) {
+
+                    const { status, error: errorResponse } = handleError(err);
+
+                    throw ctx.error(status, errorResponse);
+               }
+
+          }, {
+          detail: {
+               tags: ['Auth'],
+               summary: 'Request to check if a password reset request is valid',
+          },
+          query: t.Object({
+               token: t.String(),
+               userId: t.String({ format: 'uuid' })
+          }),
+     })
+
+     .post(
+          '/changePassword',
+
+          async (ctx) => {
+               try {
+                    const { token, userId, password } = ctx.body;
+
+                    await ctx.authServices.changePassword({ token, userId, password });
+
+                    return sendResponse(ctx, 200, "Mot de passe changé avec succès");
+
+               } catch (err) {
+                    const { status, error: errorResponse } = handleError(err);
+
+                    throw ctx.error(status, errorResponse);
+               }
+          },
+          {
+               body: t.Object({
+                    token: t.String(),
+                    userId: t.String({ format: 'uuid' }),
+                    password: t.String({
+                         error: 'Le mot de passe est invalide, il doit comporter au moins 8 caractères, dont une majuscule, un chiffre et un caractère spécial (@$!%*?&)',
+                         pattern: '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,30}$'
+
+                    })
+               }),
+               detail: {
+                    tags: ['Auth'],
+                    summary: 'Request to change a user\'s password',
+               }
+          }
+     )
 
 export const authLogged = new Elysia({ prefix: "/auth" })
 
