44a22120c5f6f75f587a71c45f05aaa40584b364
PaulDelamare <paulodela2006@gmail.com>
2025-08-26 16:35:00 +0200

    Ajout de la fonction formatDateWithoutTime pour formater une date sans l'heure.

--- src/lib/utils/formatDateError/formatDateError.ts
+++ src/lib/utils/formatDateError/formatDateError.ts
@@ -18,3 +18,12 @@ export const formatDate = (date: string | Date) => {
 
      return `${day}/${month}/${year}-${hours}h${minutes}`;
 }
+
+export const formatDateWithoutTime = (date?: Date | string) => {
+    if (!date) return "";
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, "0");
+    const month = String(d.getMonth() + 1).padStart(2, "0"); // mois = 0-11
+    const year = d.getFullYear();
+    return `${day}/${month}/${year}`;
+}
